033b4981c1618f060ec58a3beef30d1f08abd62a
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2020-05-12 14:47:53 -0300

    Refactor route

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -28,10 +28,6 @@ defmodule Servy.Handler do
 
   def log(conv), do: IO.inspect(conv)
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
-
   def track(%{status: 404, path: path} = conv) do
     IO.puts "Warning: #{path} is one the loose!"
     conv
@@ -51,20 +47,24 @@ defmodule Servy.Handler do
 
   def rewrite_path(conv), do: conv
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "Teddy" }
   end
 
-  def route(conv, "GET", "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "Hello World" }
   end
 
-  def route(conv, "GET", "/bears/" <> 1) do
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}" }
   end
 
-  def route(conv, _method, path) do
+  def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
   end
 
   def emojify(%{ status: 200 } = conv) do
